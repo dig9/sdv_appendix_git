2f4f77ecef468bcb193d4e964267884714b01b7b
Jeong-il Choi <jichoi25@kookmin.ac.kr>
2025-07-31 14:23:31 +0900

    i am the world

--- main.cpp
+++ main.cpp
@@ -7,8 +7,10 @@ using namespace std;
 int main(void)
 {
     string stringHello = "Hello World!!!";
+    string stringIam = "I am the World!!!";
 
     cout << stringHello << endl;
+    cout << stringIam << endl;
 
     return 0;
 }
